ea7b9bc3041b7f7e45dacacf10f0a29786b01703
Vincenzo Innocente <vincenzo.innocente@cern.ch>
2025-10-05 15:10:58 +0200

    clean limits

--- include/TwoFloatLimits.h
+++ include/TwoFloatLimits.h
@@ -13,48 +13,48 @@ class numeric_limits <TwoFloat<float>> {
 
     // General -- meaningful for all specializations.
 
-    static const bool is_specialized = true;
-    static FF min ()
+    static constexpr  bool is_specialized = true;
+    static FF min()
         {return nlF::min();}
-    static FF max ()
+    static FF max()
         {return nlF::max();}
-    static const int radix = 2;
-    static const int digits = nlF::digits;   
-    static const int digits10 = nlF::digits10;
-    static const int max_digits10 = nlF::max_digits10;
-    static const bool is_signed        = true;
-    static const bool is_integer    = false;
-    static const bool is_exact        = false;
-    static const bool traps            = false;
-    static const bool is_modulo        = false;
-    static const bool is_bounded    = true;
+    static constexpr  int radix = 2;
+    static constexpr  int digits = nlF::digits;   
+    static constexpr  int digits10 = nlF::digits10;
+    static constexpr  int max_digits10 = nlF::max_digits10;
+    static constexpr  bool is_signed       = true;
+    static constexpr  bool is_integer      = false;
+    static constexpr  bool is_exact        = false;
+    static constexpr  bool traps           = false;
+    static constexpr  bool is_modulo       = false;
+    static constexpr  bool is_bounded      = true;
 
     // Floating point specific.
 
-    static FF epsilon ()
-        {return nlF::epsilon();} 
-    static FF round_error ()
+    static constexpr  int min_exponent10 = nlF::min_exponent10;
+    static constexpr  int max_exponent10 = nlF::max_exponent10;
+    static constexpr  int min_exponent   = nlF::min_exponent;
+    static constexpr  int max_exponent   = nlF::max_exponent;
+
+    static constexpr  bool has_infinity             = true;
+    static constexpr  bool has_quiet_NaN            = true;
+    static constexpr  bool has_signaling_NaN        = true;
+    static constexpr  bool is_iec559                = false;
+    static constexpr  bool has_denorm               = denorm_present;
+    static constexpr  bool tinyness_before          = false;
+    static constexpr  float_round_style round_style = round_to_nearest;
+
+    static constexpr FF epsilon()
+        {return nlF::epsilon();}
+    static constexpr FF round_error()
         {return nlF::round_error();}
-    static const int min_exponent10 = nlF::min_exponent10;
-    static const int max_exponent10 = nlF::max_exponent10;
-    static const int min_exponent   = nlF::min_exponent;
-    static const int max_exponent   = nlF::max_exponent;
-
-    static const bool has_infinity            = true;
-    static const bool has_quiet_NaN            = true;
-    static const bool has_signaling_NaN        = true;
-    static const bool is_iec559                = false;
-    static const bool has_denorm            = denorm_present;
-    static const bool tinyness_before        = false;
-    static const float_round_style round_style = round_to_nearest;
-
-    static FF denorm_min ()
+    static constexpr FF denorm_min()
         {return nlF::denorm_min();}
-    static FF infinity ()
+    static constexpr FF infinity()
         {return nlF::infinity();}
-    static FF quiet_NaN ()
+    static constexpr FF quiet_NaN()
         {return  nlF::quiet_NaN();}
-    static FF signaling_NaN ()
+    static constexpr FF signaling_NaN()
         {return nlF::signaling_NaN();}
  };
 
@@ -68,48 +68,50 @@ class numeric_limits <TwoFloat<double>> {
 
     // General -- meaningful for all specializations.
 
-    static const bool is_specialized = true;
-    static FF min ()
+    static constexpr  bool is_specialized = true;
+    static constexpr FF min()
         {return nlF::min();}
-    static FF max ()
+    static constexpr FF lowest()
+        {return nlF::lowest();}
+    static constexpr FF max()
         {return nlF::max();}
-   static const int radix = 2;
-    static const int digits = nlF::digits;
-    static const int digits10 = nlF::digits10;
-    static const int max_digits10 = nlF::max_digits10;
-    static const bool is_signed        = true;
-    static const bool is_integer    = false;
-    static const bool is_exact        = false;
-    static const bool traps            = false;
-    static const bool is_modulo        = false;
-    static const bool is_bounded    = true;
+    static constexpr  int radix = 2;
+    static constexpr  int digits = nlF::digits;
+    static constexpr  int digits10 = nlF::digits10;
+    static constexpr  int max_digits10 = nlF::max_digits10;
+    static constexpr  bool is_signed        = true;
+    static constexpr  bool is_integer       = false;
+    static constexpr  bool is_exact         = false;
+    static constexpr  bool traps            = false;
+    static constexpr  bool is_modulo        = false;
+    static constexpr  bool is_bounded       = true;
 
     // Floating point specific.
 
-    static FF epsilon ()
+    static constexpr  int min_exponent10 = nlF::min_exponent10;
+    static constexpr  int max_exponent10 = nlF::max_exponent10;
+    static constexpr  int min_exponent   = nlF::min_exponent;
+    static constexpr  int max_exponent   = nlF::max_exponent;
+
+    static constexpr  bool has_infinity             = true;
+    static constexpr  bool has_quiet_NaN            = true;
+    static constexpr  bool has_signaling_NaN        = true;
+    static constexpr  bool is_iec559                = false;
+    static constexpr  bool has_denorm               = denorm_present;
+    static constexpr  bool tinyness_before          = false;
+    static constexpr  float_round_style round_style = round_to_nearest;
+
+    static constexpr FF epsilon()
         {return nlF::epsilon();}
-    static FF round_error ()
+    static constexpr FF round_error()
         {return nlF::round_error();}
-    static const int min_exponent10 = nlF::min_exponent10;
-    static const int max_exponent10 = nlF::max_exponent10;
-    static const int min_exponent   = nlF::min_exponent;
-    static const int max_exponent   = nlF::max_exponent;
-
-    static const bool has_infinity            = true;
-    static const bool has_quiet_NaN            = true;
-    static const bool has_signaling_NaN        = true;
-    static const bool is_iec559                = false;
-    static const bool has_denorm            = denorm_present;
-    static const bool tinyness_before        = false;
-    static const float_round_style round_style = round_to_nearest;
-
-    static FF denorm_min ()
+    static constexpr FF denorm_min()
         {return nlF::denorm_min();}
-    static FF infinity ()
+    static constexpr FF infinity()
         {return nlF::infinity();}
-    static FF quiet_NaN ()
+    static constexpr FF quiet_NaN()
         {return  nlF::quiet_NaN();}
-    static FF signaling_NaN ()
+    static constexpr FF signaling_NaN()
         {return nlF::signaling_NaN();}
  };
 
